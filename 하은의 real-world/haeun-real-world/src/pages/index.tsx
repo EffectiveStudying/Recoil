a571894ea5817fda904307f22254e7d9f01abbed
HAEUN KIM <ha-eun.kim@navercorp.com>
2022-02-08 01:37:24 +0900

    Home 컴포넌트 UI

---
+++ 하은의 real-world/haeun-real-world/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Home from "./Home/Home";
+import Footer from "../blocks/Footer";
+import Header from "../blocks/Header";
+
+const index = () => {
+    return (
+        <>
+            <Header />
+            <Home />
+            <Footer />
+        </>
+    );
+}
+
+export default index;
