08851487d4cd392a6393f7037cc2732fb6e9c3f1
Haeun Kim <ha-eun.kim@navercorp.com>
2022-03-13 18:37:43 +0900

    Home 컴포넌트 Page 로 전환

--- 하은의 real-world/haeun-real-world/src/components/index.tsx
+++ 하은의 real-world/haeun-real-world/src/components/index.tsx
@@ -3,4 +3,3 @@ export * from "./Header";
 export * from "./Tags";
 export * from "./Articles";
 export * from "./Tabs";
-export * from "./Home";
--- 하은의 real-world/haeun-real-world/src/pages/index.tsx
+++ 하은의 real-world/haeun-real-world/src/pages/index.tsx
@@ -1,13 +1,30 @@
-import { Footer, Header, Home } from "../components";
+import { Articles, Footer, Header, Tabs, Tags } from "../components";
+import type { NextPage } from 'next';
 
-const index = () => {
+const Home: NextPage = () => {
     return (
         <>
             <Header />
-            <Home />
+            <div className="home-page">
+            <div className="banner">
+                <div className="container">
+                    <h1 className="logo-font">conduit</h1>
+                    <p>A place to share your knowledge.</p>
+                </div>
+            </div>
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-9">
+                        <Tabs />
+                        <Articles />
+                    </div>
+                    <Tags />
+                </div>
+            </div>
+        </div>
             <Footer />
         </>
     );
 }
 
-export default index;
+export default Home;
